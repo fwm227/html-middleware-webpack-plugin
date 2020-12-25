8447f4261398e160f8636e955238612a68ae3a3f
fwm227 <clm960227@gmail.com>
2020-12-25 16:24:01 +0800

    feat: add scripts and customTags in config option

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 class htmlMiddlewareWebpackPlugin {
   constructor (options) {
+    this.initHintValue();
+    this.initOptions(options);
+    this.initHandlerFactory()
+  }
+
+  initHintValue () {
+    this.relValues = ['dns-prefetch', 'preconnect', 'prefetch', 'preload', 'prerender'];
+  }
+
+  initOptions (options) {
     if (Object.prototype.toString.apply(options) === '[object Object]') {
-      this.relValues = ['dns-prefetch', 'preconnect', 'prefetch', 'preload', 'prerender']
-      this.customOptions = options
+      this.customOptions = options;
+    }
+  }
+
+  initHandlerFactory () {
+    this.handlers = {
+      resourceHint: this.insertHintResource,
+      scripts: this.insertScripts,
+      customTags: this.insertCustomTags
     }
   }
 
@@ -12,26 +29,50 @@ class htmlMiddlewareWebpackPlugin {
       .find(constructor => constructor && constructor.name === 'HtmlWebpackPlugin');
     compiler.hooks.make.tapPromise('htmlMiddlewareWebpackPlugin', async compilation => {
       HtmlWebpackPlugin.getHooks(compilation).alterAssetTagGroups.tapAsync('htmlMiddlewareWebpackPlugin', (htmlPluginData, htmlWebpackPluginCallback) => {
-        this.executeHandleHtml(htmlPluginData)
-        htmlWebpackPluginCallback(null, htmlPluginData)
+        this.executeMiddleware(htmlPluginData);
+        htmlWebpackPluginCallback(null, htmlPluginData);
       })
     })
   }
 
-  executeHandleHtml (pluginData) {
-    Object.keys(this.customOptions).forEach(optionKey => {
-      if (~this.relValues.findIndex(el => el === optionKey)) {
-        this.customOptions[optionKey].forEach(val => {
-          pluginData.headTags.push({
-            tagName: 'link',
-            voidTag: true,
-            attributes: {
-              rel: optionKey,
-              href: val
-            }
-          })
-        })
-      }
+  insertHintResource (htmlPluginData, option_name) {
+    let attrs = null
+    Object.keys(this.customOptions[option_name]).forEach(hint_val => {
+      (this.relValues.findIndex(val => val === hint_val) > -1) && this.customOptions[option_name][hint_val].forEach(href => {
+        attrs = {
+          rel: hint_val,
+          href
+        };
+        this.insertTags(htmlPluginData, 'headTags', 'link', true, attrs);
+      })
+    })
+  }
+
+  insertScripts (htmlPluginData, option_name) {
+    this.customOptions[option_name].forEach(script => {
+      this.insertTags(htmlPluginData, 'bodyTags', 'script', false, script);
+    })
+  }
+
+  insertCustomTags (htmlPluginData, option_name) {
+    Object.keys(this.customOptions[option_name]).forEach(tag_pos => {
+      this.customOptions[option_name][tag_pos].forEach(tag_info => {
+        this.insertTags(htmlPluginData, tag_pos, tag_info.tagName, tag_info.voidTag, tag_info.attributes);
+      });
+    })
+  }
+
+  insertTags (htmlPluginData, position, tagName, voidTag, attrs) {
+    htmlPluginData[position].push({
+      tagName: tagName,
+      voidTag: voidTag,
+      attributes: attrs
+    });
+  }
+
+  executeMiddleware (htmlPluginData) {
+    Object.keys(this.customOptions).forEach(option_name => {
+      this.handlers.hasOwnProperty(option_name) && this.handlers[option_name].call(this, htmlPluginData, option_name);
     })
   }
 }
